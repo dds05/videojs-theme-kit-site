db697248d7a21a70e76b682157ffa631758749c4
Damandeep Singh <damandeep.in@gmail.com>
2025-07-13 23:33:04 +0530

    script added

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -90,7 +90,28 @@ export default function Playground() {
 
 
     useEffect(() => {
+        const script = document.createElement("script",);
+        script.setAttribute('data-name','BMC-Widget')
+        script.src = "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        script.setAttribute('data-id', 'boulderproblems');
+        script.setAttribute('data-description', 'Thank you for your support!');
+        script.setAttribute('data-message', 'This web is free to use. Do you want to help supporting it?');
+        script.setAttribute('data-color',"#FF5F5F")
+        script.setAttribute('data-position','right')
+        script.setAttribute('data-x_margin','18')
+        script.setAttribute('data-y-margin','18')
+        script.async = true
+
+        script.onload=function(){
+            var evt = document.createEvent('Event');  
+            evt.initEvent('DOMContentLoaded', false, false);  
+            window.dispatchEvent(evt);
+        }
+        document.head.appendChild(script)
         initiatePlayer();
+        return()=>{
+            document.head.removeChild(script);
+        }
     }, []);
 
 
